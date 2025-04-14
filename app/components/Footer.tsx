36ce770918e6f040c690cb430166084528ce4240
Zach Rose <zach@windmountainsoftware.com>
2025-04-14 15:05:34 -0400

    Remove underlines from footer links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,9 +2,9 @@ export default function Footer() {
     return (
         <footer className="text-center text-xs pb-2">
             <p>
-                Powered by <a href="https://use-fireproof.com" className="cursor-pointer underline">Fireproof</a>
+                Powered by <a href="https://use-fireproof.com" className="cursor-pointer">Fireproof</a>
                 &ensp;&ndash;&ensp;Join
-                us on <a href="https://discord.gg/xC55bhgf6n" className="cursor-pointer underline">Discord</a>
+                us on <a href="https://discord.gg/xC55bhgf6n" className="cursor-pointer">Discord</a>
             </p>
         </footer>
     )
