0900c2138fc4273944201da4ba294fb5b3ae6a0a
J Chris Anderson <jchris@fireproof.storage>
2025-05-09 07:19:36 -0700

    refactor: replace hue animation with colorway system in DIY logo components

---
+++ app/components/colorways.ts
@@ -0,0 +1,67 @@
+export const dark = {
+  "butter": {
+    "main": "#212A4A",
+    "gleam": "white",
+    "diy": "#FFCF5F"
+  },
+  "bubblegum": {
+    "main": "#212A4A",
+    "gleam": "white",
+    "diy": "#F077AA"
+  },
+  "marigold": {
+    "main": "#212A4A",
+    "gleam": "white",
+    "diy": "#E9AD21"
+  },  
+  "shamrock": {
+    "main": "#212A4A",
+    "gleam": "white",
+    "diy": "#01A947"
+  },
+  "tiger": {
+    "main": "#212A4A",
+    "gleam": "white",
+    "diy": "#FF571E"
+  },
+  "mono": {
+    "main": "#212A4A",
+    "gleam": "white",
+    "diy": "white"
+  }
+}
+
+export const light = {
+  "butter": {
+    "main": "#212A4A",
+    "gleam": "white",
+    "diy": "#FFCF5F"
+  },
+  "bubblegum": {
+    "main": "#212A4A",
+    "gleam": "white",
+    "diy": "#F077AA"
+  },
+  "marigold": {
+    "main": "#212A4A",
+    "gleam": "white",
+    "diy": "#E9AD21"
+  },
+  "shamrock": {
+    "main": "#01A947",
+    "gleam": "white",
+    "diy": "#212A4A"
+  },
+  "tiger": {
+    "main": "#212A4A",
+    "gleam": "white",
+    "diy": "#FF571E"
+  },
+  "mono": {
+    "main": "black",
+    "gleam": "white",
+    "diy": "white"
+  }
+}
+
+
